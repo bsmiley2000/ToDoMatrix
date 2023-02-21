50d1702264f8529edf5abb97a5451d4bebd6fe5a
kncook <katherine.n.cook3@gmail.com>
2023-02-21 13:47:33 -0700

    task file

---
+++ ToDoMatrix/Views/Home/Task.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoMatrix.Views.Home
+{
+    public class TaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
